a23fcf21d2b8f854e0355e835eba4716e1c41e0d
thaylabebber <thayla.bebber@escola.pr.gov.br>
2026-03-03 15:00:46 +0000

    ok

--- ex17.js
+++ ex17.js
@@ -1,6 +1,15 @@
 17//criar um programa que mostre conceitos A B C ou D e de acordo com as notas: >9.4 e 8.5 B; Entre 8.4 e 7.0 C; <6.9 D 
-let nota = 7.0
-if (`nota >= 9.5`)
+let nota = 7.7
+
+if (`nota >= 9.5`){
+    console.log("A");
+}else if (nota >= 9.4 && nota <= 8.5) {
+    console.log("B");
+}else if (nota >=8.4 && nota <= 7.0) {
+    console.log("C");
+}else{
+    console.log("D");
+}
 
 
 
